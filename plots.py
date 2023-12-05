7977b345ccb1297ede76574c5170dad5f53b2db0
kellele4 <kellele4@students.zhaw.ch>
2023-12-04 22:54:19 -0500

    update average over 30

--- plots.py
+++ plots.py
@@ -2,17 +2,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the CSV file
-file_path = 'results14.csv'
-data = pd.read_csv(file_path)
+data = pd.read_csv('results_final.csv')  # Replace with your file path
 
-x = data.iloc[:, 0]
-y = data.iloc[:, 1]
+# Calculate averages for every 50 entries in the second column
+averages = []
+for i in range(0, len(data), 50):
+    avg = data.iloc[i:i+30, 1].mean()  # Assuming second column is at index 1
+    averages.append(avg)
 
+# Plotting the averages
 plt.figure(figsize=(10, 6))
-plt.plot(x, y, marker='o', linestyle='-')
-plt.title('Performance over time')
-plt.xlabel('Number of runs')
-plt.ylabel('Number of pipes passed')
+plt.plot(averages, marker=',', linestyle='-')
+plt.title('Average of Every 50 Entries from high score')
+plt.xlabel('Episodes (Each Represents 50 Episodes)')
+plt.ylabel('Average Value')
 plt.grid(True)
 plt.show()
 plt.savefig('performange.png')
